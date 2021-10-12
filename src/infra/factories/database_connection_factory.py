8d02b566900e5259713deeacef130d598b930520
João Victor Teixeira De Oliveira <joliveira@acesso.com>
2021-10-12 14:28:35 -0300

    Fix env variable name

--- src/infra/factories/database_connection_factory.py
+++ src/infra/factories/database_connection_factory.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 def create():
-    database_url = os.getenv('DbContextPython')
+    database_url = os.getenv('DATABASE_URL')
     print(database_url)
     engine = create_engine(database_url, echo=True)
 
