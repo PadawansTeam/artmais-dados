0ef6f94e4d22a0b903b731e9ac113a06c0f7f8e5
plug <renancoelho.dev@gmail.com>
2021-10-19 01:46:48 -0300

    Fix bug numero de likes

--- src/util/data_util.py
+++ src/util/data_util.py
@@ -7,60 +7,74 @@ from sklearn import linear_model
 
 def month_growth(df):
 
-    df['sum'] = df['idpublicacao'].groupby(
-        df['date']).transform('sum')
+    try:
+        df['sum'] = df['idpublicacao'].groupby(
+            df['date']).transform('sum')
 
-    df = df.drop_duplicates('date')
+        df = df.drop_duplicates('date')
 
-    df = df.drop('idpublicacao', 1)
+        df = df.drop('idpublicacao', 1)
 
-    return df.to_dict('records')
+        return df.to_dict('records')
+
+    except(Exception):
+        return None
 
 def users_age_average(df):
-    df['date'] = pd.to_datetime(df['date'])
-    df['now'] = pd.to_datetime(df['now'])
-    df = df.drop_duplicates(['idusuario'])
-    df['timedelta'] = ((df['now'] - df['date']) / np.timedelta64(1, 'Y'))
 
-    return df
+    try:
+        df['date'] = pd.to_datetime(df['date'])
+        df['now'] = pd.to_datetime(df['now'])
+        df = df.drop_duplicates(['idusuario'])
+        df['timedelta'] = ((df['now'] - df['date']) / np.timedelta64(1, 'Y'))
+
+        return df
+
+    except(Exception):
+        return 0
 
 def linear_regression(dict):
-    lm = linear_model.LinearRegression()
 
-    df_dict = pd.DataFrame.from_dict(dict)
+    try:
+        lm = linear_model.LinearRegression()
 
-    df_date = pd.DataFrame(df_dict['date'])
-    df_sum = pd.DataFrame(df_dict['sum'])
+        df_dict = pd.DataFrame.from_dict(dict)
 
-    df_date['date'] = pd.to_datetime(df_date['date'])
+        df_date = pd.DataFrame(df_dict['date'])
+        df_sum = pd.DataFrame(df_dict['sum'])
 
-    df_date['date'] = df_date['date'].map(datetime.datetime.toordinal)
+        df_date['date'] = pd.to_datetime(df_date['date'])
 
-    model = lm.fit(df_date, df_sum)
+        df_date['date'] = df_date['date'].map(datetime.datetime.toordinal)
 
-    month_list = []
+        model = lm.fit(df_date, df_sum)
 
-    for i in range(1, 6):
-        month_list.append((datetime.date.today() + relativedelta(months=+i)).strftime('%m/%Y'))
+        month_list = []
 
-    X = pd.DataFrame(month_list, columns=['date'])
-    X['date'] = pd.to_datetime(X['date'])
-    X['date'] = X['date'].map(datetime.datetime.toordinal)
+        for i in range(1, 6):
+            month_list.append((datetime.date.today() + relativedelta(months=+i)).strftime('%m/%Y'))
 
-    Y = model.predict(X)
-    Y = pd.DataFrame(Y, columns=['prediction'])
-    df = pd.concat([X, Y], axis=1)
+        X = pd.DataFrame(month_list, columns=['date'])
+        X['date'] = pd.to_datetime(X['date'])
+        X['date'] = X['date'].map(datetime.datetime.toordinal)
 
-    df['date'] = df['date'].map(datetime.datetime.fromordinal)
-    df['date'] = df['date'].dt.strftime("%m/%Y")
+        Y = model.predict(X)
+        Y = pd.DataFrame(Y, columns=['prediction'])
+        df = pd.concat([X, Y], axis=1)
 
-    df = df.astype({'prediction': int})
+        df['date'] = df['date'].map(datetime.datetime.fromordinal)
+        df['date'] = df['date'].dt.strftime("%m/%Y")
 
-    df['prediction'] = np.where(df.prediction < 0, 0, df.prediction)
+        df = df.astype({'prediction': int})
 
-    linear_regression_dict = df.to_dict('records')
+        df['prediction'] = np.where(df.prediction < 0, 0, df.prediction)
 
-    return linear_regression_dict
+        linear_regression_dict = df.to_dict('records')
+
+        return linear_regression_dict
+
+    except(Exception):
+        return None
 
 def total_interacoes(dict):
     df_dict = pd.DataFrame.from_dict(dict)
