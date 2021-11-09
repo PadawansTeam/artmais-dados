6265462c998b2b3b44468b55fccd55a7cf4c896d
plug <renancoelho.dev@gmail.com>
2021-11-08 23:56:29 -0300

    Ajuste paths e adicao testes recomendacao

---
+++ src/tests/recomendation/test_prediction.py
@@ -0,0 +1,14 @@
+import unittest
+from src.app.recomendation.recomendation import Recomendation
+
+
+class TestPrediction(unittest.TestCase):
+    def test_prediction_recommendation(self):
+
+        r = Recomendation()
+
+        self.assertEqual(r.prediction_result(7), [8,11,6,9,7])
+
+
+if __name__ == '__name__':
+    unittest.main()
--- src/tests/util/test_datautil.py
+++ src/tests/util/test_datautil.py
@@ -1,9 +1,46 @@
 import unittest
-from src.util.data_util import total_average
+from src.util.data_util import month_growth, linear_regression, total_average
 import pandas as pd
 
 
-class TestAge(unittest.TestCase):
+class TestGrowth(unittest.TestCase):
+    def test_month_grow(self):
+        dict = {"date": {"0": "11/2021", "1": "11/2021", "2": "11/2021", "3": "11/2021", "4": "11/2021",
+                                  "5": "11/2021", "6": "11/2021", "7": "11/2021"},
+                         "idpublicacao": {"0": 1, "1": 1, "2": 1, "3": 1, "4": 1, "5": 1, "6": 1, "7": 1}}
+
+        df = pd.DataFrame.from_dict(dict)
+
+        result = [{"date": "09/2021", "sum": 0}, {"date": "10/2021", "sum": 0}, {"date": "11/2021", "sum": 8}]
+
+        self.assertEqual(month_growth(df), result)
+
+    def test_month_grow_with_no_values(self):
+        dict = {}
+
+        df = pd.DataFrame.from_dict(dict)
+
+        self.assertEqual(month_growth(df), None)
+
+    def test_month_grow_with_value_current_month(self):
+        dict = {"date": {"0": "11/2021"},
+                         "idpublicacao": {"0": 1}}
+
+        df = pd.DataFrame.from_dict(dict)
+
+        result = [{"date": "09/2021", "sum": 0}, {"date": "10/2021", "sum": 0}, {"date": "11/2021", "sum": 1}]
+
+        self.assertEqual(month_growth(df), result)
+
+    def test_month_grow_with_value_three_months_ago(self):
+        dict = {"date": {"0": "09/2021"},
+                         "idpublicacao": {"0": 1}}
+
+        df = pd.DataFrame.from_dict(dict)
+
+        result = [{"date": "09/2021", "sum": 1}, {"date": "10/2021", "sum": 0}, {"date": "11/2021", "sum": 0}]
+
+        self.assertEqual(month_growth(df), result)
 
     def test_age(self):
         dict_comments = {"date": {"0": "Sat, 01 Jan 2000 00:00:00 GMT", "1": "Wed, 05 Jul 2000 00:00:00 GMT",
@@ -75,6 +112,51 @@ class TestAge(unittest.TestCase):
 
         self.assertEqual(total_average(df_comments, df_likes), 20)
 
+    def test_linear_regression(self):
+        dict_comments = [{"date": "09/2021", "sum": 0}, {"date": "10/2021", "sum": 0}, {"date": "11/2021", "sum": 8}]
 
-if __name__ == '__main__':
+        df = pd.DataFrame.from_dict(dict_comments)
+
+        result = [{"date": "09/2021", "prediction": -1}, {"date": "10/2021", "prediction": -1},
+                  {"date": "11/2021", "prediction": -1},
+                  {"date": "12/2021", "prediction": 10}, {"date": "01/2022", "prediction": 14},
+                  {"date": "02/2022", "prediction": 18}]
+
+        self.assertEqual(linear_regression(df), result)
+
+    def test_linear_regression_with_null_dataframe(self):
+        dict_comments = []
+
+        df = pd.DataFrame.from_dict(dict_comments)
+
+        result = None
+
+        self.assertEqual(linear_regression(df), result)
+
+    def test_linear_regression_with_same_values(self):
+        dict_comments = [{"date": "09/2021", "sum": 7}, {"date": "10/2021", "sum": 7}, {"date": "11/2021", "sum": 7}]
+
+        df = pd.DataFrame.from_dict(dict_comments)
+
+        result = [{"date": "09/2021", "prediction": -1}, {"date": "10/2021", "prediction": -1},
+                  {"date": "11/2021", "prediction": -1},
+                  {"date": "12/2021", "prediction": 7}, {"date": "01/2022", "prediction": 7},
+                  {"date": "02/2022", "prediction": 7}]
+
+        self.assertEqual(linear_regression(df), result)
+
+    def test_linear_regression_with_zero(self):
+        dict_comments = [{"date": "09/2021", "sum": 0}, {"date": "10/2021", "sum": 0}, {"date": "11/2021", "sum": 0}]
+
+        df = pd.DataFrame.from_dict(dict_comments)
+
+        result = [{"date": "09/2021", "prediction": -1}, {"date": "10/2021", "prediction": -1},
+                  {"date": "11/2021", "prediction": -1},
+                  {"date": "12/2021", "prediction": 0}, {"date": "01/2022", "prediction": 0},
+                  {"date": "02/2022", "prediction": 0}]
+
+        self.assertEqual(linear_regression(df), result)
+
+
+if __name__ == '__name__':
     unittest.main()
